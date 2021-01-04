b974de021fb55b0d79df81b862e0f30d458027e9
Niko-7 <nfloros92@gmail.com>
2021-01-04 12:22:41 +0000

    works on game logic

--- Components/Shapes.js
+++ Components/Shapes.js
@@ -1,20 +1,44 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Shapes = ({ screenSize }) => {
   const handlePress = (color) => {
     console.log(color);
   };
 
+  const getRandomPanel = () => {
+    const panels = ['red', 'yellow', 'blue', 'green'];
+    return panels[parseInt(Math.random() * panels.length)];
+  };
+
+  const sequence = [getRandomPanel()];
+  let sequenceToGuess = [...sequence];
+
   return (
     <View style={styles.rowContainer}>
       <View style={styles.topRow}>
-        <Text style={styles.redSeg} onPress={() => handlePress("red")} />
-        <Text style={styles.yellowSeg} onPress={() => handlePress("yellow")} />
+        <Text
+          nativeID='red'
+          style={styles.redSeg}
+          onPress={() => handlePress('red')}
+        />
+        <Text
+          nativeID='yellow'
+          style={styles.yellowSeg}
+          onPress={() => handlePress('yellow')}
+        />
       </View>
       <View style={styles.bottomRow}>
-        <Text style={styles.blueSeg} onPress={() => handlePress("blue")} />
-        <Text style={styles.greenSeg} onPress={() => handlePress("green")} />
+        <Text
+          nativeID='blue'
+          style={styles.blueSeg}
+          onPress={() => handlePress('blue')}
+        />
+        <Text
+          nativeID='green'
+          style={styles.greenSeg}
+          onPress={() => handlePress('green')}
+        />
       </View>
     </View>
   );
@@ -23,53 +47,53 @@ const Shapes = ({ screenSize }) => {
 const styles = StyleSheet.create({
   rowContainer: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   topRow: {
     flex: 1,
-    flexDirection: "row",
-    alignItems: "flex-end",
+    flexDirection: 'row',
+    alignItems: 'flex-end',
   },
   bottomRow: {
     flex: 1,
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   redSeg: {
     width: 150,
     height: 150,
-    backgroundColor: "red",
+    backgroundColor: 'red',
     borderTopLeftRadius: 150,
-    borderColor: "black",
-    borderStyle: "solid",
+    borderColor: 'black',
+    borderStyle: 'solid',
     borderWidth: 3,
   },
   yellowSeg: {
     width: 150,
     height: 150,
-    backgroundColor: "yellow",
+    backgroundColor: 'yellow',
     borderTopRightRadius: 150,
-    borderColor: "black",
-    borderStyle: "solid",
+    borderColor: 'black',
+    borderStyle: 'solid',
     borderWidth: 3,
   },
   blueSeg: {
     width: 150,
     height: 150,
-    backgroundColor: "blue",
+    backgroundColor: 'blue',
     borderBottomLeftRadius: 150,
-    borderColor: "black",
-    borderStyle: "solid",
+    borderColor: 'black',
+    borderStyle: 'solid',
     borderWidth: 3,
   },
   greenSeg: {
     width: 150,
     height: 150,
-    backgroundColor: "green",
+    backgroundColor: 'green',
     borderBottomRightRadius: 150,
-    borderColor: "black",
-    borderStyle: "solid",
+    borderColor: 'black',
+    borderStyle: 'solid',
     borderWidth: 3,
   },
 });
