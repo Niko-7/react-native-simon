5c2da10ae34c7412e04b2c4022876e388e310992
Sam Parry <samparrymusic@gmail.com>
2021-01-04 10:54:48 +0000

    created react native app, began Simon game

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Shapes from "./Components/Shapes";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Shapes />
     </View>
   );
 }
@@ -14,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Components/Shapes.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Shapes = () => {
+  const handlePress = (color) => {
+    console.log(color);
+  };
+
+  return (
+    <View style={styles.rowContainer}>
+      <View style={styles.topRow}>
+        <Text style={styles.redSeg} onPress={() => handlePress("red")} />
+        <Text style={styles.yellowSeg} onPress={() => handlePress("yellow")} />
+      </View>
+      <View style={styles.bottomRow}>
+        <Text style={styles.blueSeg} onPress={() => handlePress("blue")} />
+        <Text style={styles.greenSeg} onPress={() => handlePress("green")} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  rowContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  topRow: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-end",
+  },
+  bottomRow: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  redSeg: {
+    width: 150,
+    height: 150,
+    backgroundColor: "red",
+    borderTopLeftRadius: 150,
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 3,
+  },
+  yellowSeg: {
+    width: 150,
+    height: 150,
+    backgroundColor: "yellow",
+    borderTopRightRadius: 150,
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 3,
+  },
+  blueSeg: {
+    width: 150,
+    height: 150,
+    backgroundColor: "blue",
+    borderBottomLeftRadius: 150,
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 3,
+  },
+  greenSeg: {
+    width: 150,
+    height: 150,
+    backgroundColor: "green",
+    borderBottomRightRadius: 150,
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 3,
+  },
+});
+
+export default Shapes;
