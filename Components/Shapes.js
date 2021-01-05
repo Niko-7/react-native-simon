a0dd3cd456b325c6487e62e0f07c5eaf0b4ad666
Sam Parry <samparrymusic@gmail.com>
2021-01-05 10:43:01 +0000

    resolved async issue with flash function

--- Components/Shapes.js
+++ Components/Shapes.js
@@ -1,93 +1,100 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const Shapes = () => {
-  const [canClick, setCanClick] = useState(true);
-  const [flashCol, setFlashCol] = useState('');
-  const [sequence, setSequence] = useState([]);
+  const [panels, setPanels] = useState(["red", "yellow", "blue", "green"]);
+  const [canClick, setCanClick] = useState(false);
+  const [flashCol, setFlashCol] = useState("");
+
+  const [sequence, setSequence] = useState(["red", "red"]);
+
+  const getRandomPanel = () => {
+    const panel = panels[parseInt(Math.random() * panels.length)];
+    // setSequence(["red", "red", "red", "red"]);
+    return panel;
+  };
+
+  const flash = (flashy) => {
+    return new Promise((resolve, reject) => {
+      setFlashCol(flashy);
+
+      console.log(flashy, "colour");
+      console.log("flash on");
+
+      setTimeout(() => {
+        setFlashCol();
+
+        console.log("flash off");
+        resolve();
+        setTimeout(() => {}, 250);
+      }, 1000);
+    });
+  };
+
+  // const startFlashing = () => {
+  //   sequence.forEach((flashy) => flash(flashy));
+  // };
+
+  // async await expects a promise
+  const startFlashing = async () => {
+    // canClick = false;
+    for (const panel of sequence) {
+      await flash(panel);
+    }
+    // canClick = true;
+  };
 
   const handlePress = (color) => {
     console.log(color);
   };
 
   useEffect(() => {
-    getRandomPanel();
-    console.log(sequence);
-    for (let colour of sequence) flash(colour);
-    // sequence.forEach((seq) => flash(seq));
-  }, []);
-
-  const getRandomPanel = () => {
-    const panels = ['red', 'yellow', 'blue', 'green'];
-    const panel = panels[parseInt(Math.random() * panels.length)];
-    setSequence(['red', 'red', 'red', 'red']);
-    // setSequence([...sequence, panel]);
-    // setSequence([panel, ...sequence]);
-    // console.log(sequence[0], 'in panel');
-    return panel;
-  };
-
-  const flash = (flashy) => {
-    setFlashCol(flashy);
-    // console.log('red flash');
-    setTimeout(() => {
-      setFlashCol();
-      // console.log('flash off');
-      setTimeout(() => {}, 250);
-    }, 1000);
-  };
-
-  // const sequence = [getRandomPanel()];
-  let sequenceToGuess = [sequence];
-
-  // const gameplay = (sequence) => {
-  //   for (let panel of sequence) {
-  //     flash(panel);
-  //   }
-  // };
-  // gameplay();
+    // getRandomPanel();
+    console.log(sequence, "<---- SEQUENCE");
+    startFlashing();
+  }, [sequence]);
 
   return (
     <View style={styles.rowContainer}>
       <View style={styles.topRow}>
         <Text
-          nativeID='red'
+          nativeID="red"
           style={
-            flashCol === 'red'
+            flashCol === "red"
               ? [styles.redFlash, styles.seg]
               : [styles.redSeg, styles.seg]
           }
-          onPress={() => handlePress('red')}
+          onPress={() => handlePress("red")}
         />
         <Text
-          nativeID='yellow'
+          nativeID="yellow"
           style={
-            flashCol === 'yellow'
+            flashCol === "yellow"
               ? [styles.yellowFlash, styles.seg]
               : [styles.yellowSeg, styles.seg]
           }
-          onPress={() => handlePress('yellow')}
+          onPress={() => handlePress("yellow")}
         />
       </View>
       <View style={styles.bottomRow}>
         <Text
-          nativeID='blue'
+          nativeID="blue"
           style={
-            flashCol === 'blue'
+            flashCol === "blue"
               ? [styles.blueFlash, styles.seg]
               : [styles.blueSeg, styles.seg]
           }
-          onPress={() => handlePress('blue')}
+          onPress={() => handlePress("blue")}
         />
         <Text
-          nativeID='green'
+          nativeID="green"
           style={
-            flashCol === 'green'
+            flashCol === "green"
               ? [styles.greenFlash, styles.seg]
               : [styles.greenSeg, styles.seg]
           }
-          onPress={() => handlePress('green')}
+          onPress={() => handlePress("green")}
         />
       </View>
     </View>
@@ -97,56 +104,56 @@ const Shapes = () => {
 const styles = StyleSheet.create({
   rowContainer: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
   topRow: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'flex-end',
+    flexDirection: "row",
+    alignItems: "flex-end",
   },
   bottomRow: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   redSeg: {
-    backgroundColor: 'red',
+    backgroundColor: "red",
     borderTopLeftRadius: 150,
   },
   yellowSeg: {
-    backgroundColor: 'yellow',
+    backgroundColor: "yellow",
     borderTopRightRadius: 150,
   },
   blueSeg: {
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
     borderBottomLeftRadius: 150,
   },
   greenSeg: {
-    backgroundColor: 'green',
+    backgroundColor: "green",
     borderBottomRightRadius: 150,
   },
   seg: {
     width: 150,
     height: 150,
-    borderColor: 'black',
-    borderStyle: 'solid',
+    borderColor: "black",
+    borderStyle: "solid",
     borderWidth: 3,
   },
   redFlash: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderTopLeftRadius: 150,
   },
   blueFlash: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderBottomLeftRadius: 150,
   },
   yellowFlash: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderTopRightRadius: 150,
   },
   greenFlash: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderBottomRightRadius: 150,
   },
 });
