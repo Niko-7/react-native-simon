9a6d1560e07594777ea990a1642a3bc276ab95eb
Richard Mutch <rsmutch@gmail.com>
2021-01-05 12:53:58 +0000

    Adds routing between components

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
+import 'react-native-gesture-handler';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import Menu from './Components/Menu';
 import Shapes from './Components/Shapes';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Menu />
-      {/* <Shapes /> */}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Menu"
+          component={Menu}
+          options={{ title: 'Simon Says' }}
+        />
+        <Stack.Screen name="Shapes" component={Shapes} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- Components/Menu.js
+++ Components/Menu.js
@@ -1,59 +1,80 @@
 import React from 'react';
 import { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, StyleSheet, TextInput, Button, Keyboard } from 'react-native';
 
-const Menu = () => {
+const Menu = ({ navigation }) => {
   const [username, setUsername] = useState('');
+  const [purpose, setPurpose] = useState('');
+  const [menuView, setMenuView] = useState('start');
+  const [emptyField, setEmptyField] = useState('false');
+
+  const handleUsernameNext = () => {
+    if (username.length > 0) {
+      setEmptyField(false);
+      setMenuView('purpose');
+    } else {
+      setEmptyField(true);
+    }
+  };
 
   return (
     <View style={styles.menuContainer}>
-      <View style={styles.menuTitleContainer}>
-        <Text style={styles.menuTitle}>Simon Says</Text>
-      </View>
-      <TextInput
-        style={styles.usernameInput}
-        onChangeText={(text) => setUsername(text)}
-        placeholder="What is your name?"
-      />
-      <Button title="Next" style={styles.nextButton} />
-      <View style={styles.menuFooter} />
+      {menuView === 'start' && (
+        <Button title="Start" onPress={() => setMenuView('username')} />
+      )}
+      {menuView === 'username' && (
+        <View>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(text) => setUsername(text)}
+            placeholder="What is your name?"
+            maxLength={20}
+            onBlur={Keyboard.dismiss}
+          />
+          <Button
+            title="Next"
+            style={styles.nextButton}
+            onPress={handleUsernameNext}
+          />
+        </View>
+      )}
+      {menuView === 'purpose' && (
+        <View>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(text) => setPurpose(text)}
+            placeholder="What are you playing for?"
+            maxLength={20}
+            onBlur={Keyboard.dismiss}
+          />
+          <Button
+            title="Play"
+            style={styles.nextButton}
+            onPress={() => navigation.navigate('Shapes')}
+          />
+        </View>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   menuContainer: {
-    // backgroundColor: 'red',
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     padding: 10
   },
-  menuTitleContainer: {
-    // backgroundColor: 'yellow',
-    flex: 5,
-    justifyContent: 'center'
-  },
-  menuTitle: {
-    fontSize: 'larger',
-    textDecorationLine: 'underline'
-  },
-  usernameInput: {
-    // backgroundColor: 'green',
+  textInput: {
     textAlign: 'center',
-    flex: 1,
     borderBottomWidth: 1,
     borderBottomColor: 'gray',
     marginBottom: 10,
     fontSize: 'large',
     padding: 0
   },
-  nextButton: { margin: 1 },
-  menuFooter: {
-    // backgroundColor: 'black',
-    flex: 5
-  }
+  nextButton: { margin: 1 }
 });
 
 export default Menu;
