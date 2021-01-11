194e17134d0fca19bfb2d5b83a36ae8d8c108a24
Sam Parry <samparrymusic@gmail.com>
2021-01-11 13:04:47 +0000

    begins to add multiplayer/room functionality

--- Components/WaitingRoom.js
+++ Components/WaitingRoom.js
@@ -1,40 +1,57 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
-import { useEffect } from 'react/cjs/react.development';
-import { firebase } from '../src/firebaseConfig';
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+import { useEffect } from "react/cjs/react.development";
+import { firebase } from "../src/firebaseConfig";
 
 const WaitingRoom = ({
   route: {
-    params: { user }
-  }
+    params: { user, roomCode, isHost },
+  },
 }) => {
   const [users, setUsers] = useState([user, user, user]);
-  const gamesRef = firebase.firestore().collection('multiplayerGames');
+  const [hostRoomCode, setHostRoomCode] = useState("");
+
+  const roomsRef = firebase.firestore().collection("multiplayerGames");
+
+  const generateRoomCode = () => {
+    let result = "";
+    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (let i = 0; i < 4; i++) {
+      result += letters.charAt(Math.floor(Math.random() * letters.length));
+    }
+    return result;
+  };
 
   useEffect(() => {
-    console.log(user);
-    gamesRef
-      .add({
-        createdAt: new Date().toISOString(),
-        gameIsActive: false,
-        host: user,
-        players: [user],
-        playersFinishedLevel: [],
-        winner: null
-      })
-      .then(function (docRef) {
-        firebase
-          .firestore()
-          .collection('multiplayerGames')
-          .doc(docRef.id)
-          .onSnapshot(function (doc) {
-            console.log('Current data: ', doc.data());
-          });
-      })
-      .catch(function (error) {
-        console.error('Error adding document: ', error);
-      });
+    if (isHost) {
+      const code = generateRoomCode();
+      setHostRoomCode(code);
+      roomsRef
+        .add({
+          roomCode: code,
+          createdAt: new Date().toISOString(),
+          gameIsActive: false,
+          host: user,
+          players: [user],
+          playersGameOver: [],
+          winner: null,
+        })
+        .then(function (docRef) {
+          firebase
+            .firestore()
+            .collection("multiplayerGames")
+            .doc(docRef.id)
+            .onSnapshot(function (doc) {
+              console.log("Current data: ", doc.data());
+            });
+        })
+        .catch(function (error) {
+          console.error("Error adding document: ", error);
+        });
+    } else {
+      setHostRoomCode(roomCode);
+    }
   }, []);
 
   const handleReady = () => {};
