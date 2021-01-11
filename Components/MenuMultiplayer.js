132bc72066e6fe465665f939c10ee431ae3c38cb
Peter Lindley <Yunadanta@hotmail.com>
2021-01-11 14:39:57 +0000

    Added join room functionality

--- Components/MenuMultiplayer.js
+++ Components/MenuMultiplayer.js
@@ -1,17 +1,51 @@
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Button, TextInput } from 'react-native-paper';
+import { Button, TextInput, Text } from 'react-native-paper';
+
+import { firebase } from '../src/firebaseConfig';
 
 const MenuMultiplayer = ({
   navigation,
   route: {
-    params: { user }
-  }
+    params: { user },
+  },
 }) => {
   const [roomCode, setRoomCode] = useState(null);
+  const [argument, setArgument] = useState(user.decision);
+
+  const db = firebase.firestore();
+
+  const handleSubmit = (roomCode) => {
+    db.collection('multiplayerGames')
+      .where('roomCode', '==', roomCode)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          db.collection('multiplayerGames')
+            .doc(roomCode)
+            .collection('users')
+            .doc(user.username)
+            .set({
+              username: user.username,
+              userId: user.id,
+              userImg: user.userImg,
+              score: 0,
+              argument: argument,
+              isHost: false,
+            });
+          navigation.navigate('WaitingRoom', { user, roomCode });
+        } else {
+          alert({ error: 'Room does not exist!' });
+        }
+      });
+  };
+
   return (
     <View style={styles.menuMultiplayerContainer}>
       <View>
+        <View>
+          <Text>Argument: {argument}</Text>
+        </View>
         <View style={styles.joinRoom}>
           <TextInput
             onChangeText={(text) => setRoomCode(text)}
@@ -22,7 +56,7 @@ const MenuMultiplayer = ({
             <Button
               mode="contained"
               color="blue"
-              onPress={() => navigation.navigate('Shapes', { roomCode })}
+              onPress={() => handleSubmit(roomCode)}
             >
               Join a Room
             </Button>
@@ -47,14 +81,14 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   button: {
-    margin: 5
+    margin: 5,
   },
   joinRoom: {
-    height: 100
-  }
+    height: 100,
+  },
 });
 
 export default MenuMultiplayer;
