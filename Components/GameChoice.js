e67fea3912cec5a1e7f77d7f6e5102a7d59aa282
Richard Mutch <rsmutch@gmail.com>
2021-01-12 09:41:40 +0000

    Adds high score and avatar to gamechoice

--- Components/GameChoice.js
+++ Components/GameChoice.js
@@ -1,9 +1,49 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Text, Image } from 'react-native';
 import { Button } from 'react-native-paper';
+
 import { firebase } from '../src/firebaseConfig';
+import 'firebase/storage';
+import GameHighScore from './GameHighScore';
 
 const GameChoice = ({ setUser, extraData, navigation }) => {
+  const [imageUrl, setImageUrl] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [highScore, setHighScore] = useState();
+  const scoreRef = firebase.firestore().collection('scores').doc(extraData.id);
+
+  useEffect(() => {
+    // Pulls high score for user
+    scoreRef
+      .get()
+      .then(function (doc) {
+        if (doc.exists) {
+          const data = doc.data();
+          setHighScore(data.highScore);
+        } else {
+          console.log('No such document!');
+        }
+      })
+      .catch(function (error) {
+        console.log('Error getting document:', error);
+      });
+    // attempts to load image
+    getAndLoadHttpUrl();
+  }, []);
+
+  const getAndLoadHttpUrl = async () => {
+    firebase
+      .storage()
+      .ref('/' + 'no-profile-image.png') //name in storage in firebase console
+      .getDownloadURL()
+      .then((url) => {
+        setImageUrl(url);
+      })
+      .catch((e) => console.log('Errors while downloading => ', e));
+  };
+
+  const user = extraData;
+
   const logoutPress = () => {
     firebase
       .auth()
@@ -16,11 +56,17 @@ const GameChoice = ({ setUser, extraData, navigation }) => {
         // An error happened.
       });
   };
-  const user = extraData;
   return (
     <View style={styles.userInfo}>
+      {console.log(user)}
       <View style={styles.userNameMsg}>
         <Text style={styles.userNameSize}>Welcome {user.username}</Text>
+        <GameHighScore
+          isPlaying={false}
+          currentScore={null}
+          highScore={highScore}
+        />
+        <Image style={styles.avatar} source={{ uri: imageUrl }} />
         <Button mode="contained" color="blue" onPress={logoutPress}>
           log out
         </Button>
@@ -76,6 +122,11 @@ const styles = StyleSheet.create({
     // justifyContent: 'flex-start',
     justifyContent: 'center',
     marginTop: 50
+  },
+  avatar: {
+    width: 200,
+    height: 200,
+    marginBottom: 10
   }
 });
 
