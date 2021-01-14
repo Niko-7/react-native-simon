6c440371b7258e1adef4fb78bbd3c8353941d90c
Richard Mutch <rsmutch@gmail.com>
2021-01-14 16:46:39 +0000

    Removes high score from waiting room

--- Components/WaitingRoom.js
+++ Components/WaitingRoom.js
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
-import { Button, Card, Title, Paragraph } from "react-native-paper";
-import { useEffect } from "react/cjs/react.development";
-import AppLoading from "expo-app-loading";
-import * as Font from "expo-font";
-import { firebase } from "../src/firebaseConfig";
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import { Button, Card, Title, Paragraph } from 'react-native-paper';
+import { useEffect } from 'react/cjs/react.development';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import { firebase } from '../src/firebaseConfig';
 
 const WaitingRoom = ({
   navigation,
   route: {
-    params: { user, code, roomId },
-  },
+    params: { user, code, roomId }
+  }
 }) => {
   let [fontsLoaded, error] = Font.useFonts({
-    Graduate: require("../assets/fonts/Graduate-Regular.ttf"),
+    Graduate: require('../assets/fonts/Graduate-Regular.ttf')
   });
 
   const [users, setUsers] = useState([]);
-  const [host, setHost] = useState("");
+  const [host, setHost] = useState('');
   const [imageUrl, setImageUrl] = useState();
 
   const getAndLoadHttpUrl = async () => {
@@ -28,18 +28,18 @@ const WaitingRoom = ({
       .then((url) => {
         setImageUrl(url);
       })
-      .catch((e) => console.log("Errors while downloading => ", e));
+      .catch((e) => console.log('Errors while downloading => ', e));
   };
 
   const usersRef = firebase
     .firestore()
-    .collection("multiplayerGames")
+    .collection('multiplayerGames')
     .doc(roomId)
-    .collection("users");
+    .collection('users');
 
   const roomRef = firebase
     .firestore()
-    .collection("multiplayerGames")
+    .collection('multiplayerGames')
     .doc(roomId);
 
   useEffect(() => {
@@ -60,14 +60,14 @@ const WaitingRoom = ({
       if (querySnapshot.data().gameIsActive) {
         showUsers();
         loadingRoom();
-        navigation.navigate("Game", {
+        navigation.navigate('Game', {
           users,
           user,
           roomId,
           isMultiplayer: true,
-          difficulty: "normal",
+          difficulty: 'normal',
           flashTime: 300,
-          betweenTime: 250,
+          betweenTime: 250
         });
       }
     });
@@ -86,7 +86,7 @@ const WaitingRoom = ({
         <View style={styles.headerCont}>
           <Image
             style={styles.img}
-            source={require("../assets/ARGULYMPICS.png")}
+            source={require('../assets/ARGULYMPICS.png')}
           />
           <View style={styles.titleCont}>
             <Text style={styles.pageTitleText}> Get Ready to Argue! </Text>
@@ -117,7 +117,7 @@ const WaitingRoom = ({
                     <Image
                       style={styles.avatar}
                       source={{
-                        uri: imageUrl,
+                        uri: imageUrl
                       }}
                     />
                   </View>
@@ -128,9 +128,6 @@ const WaitingRoom = ({
                       )}
                       {user.username}
                     </Title>
-                    <Paragraph style={styles.highScore}>
-                      Current High Score: {user.score}
-                    </Paragraph>
                     <Text numberOfLines={1} style={styles.arguText}>
                       Fighting for: {user.argument}
                     </Text>
@@ -150,104 +147,104 @@ export default WaitingRoom;
 const styles = StyleSheet.create({
   pageContainer: {
     flex: 1,
-    backgroundColor: "#bde0fe",
+    backgroundColor: '#bde0fe'
   },
 
   // HEADER SECTION
 
   buffer: {
     flex: 2,
-    height: 10,
+    height: 10
   },
   headerCont: {
     flex: 2,
-    justifyContent: "center",
+    justifyContent: 'center'
   },
   img: {
     flex: 3,
-    width: "100%",
-    resizeMode: "center",
+    width: '100%',
+    resizeMode: 'center'
   },
   titleCont: {
-    flex: 1,
+    flex: 1
   },
   pageTitleText: {
-    fontFamily: "Graduate",
-    textAlign: "center",
-    fontSize: 30,
+    fontFamily: 'Graduate',
+    textAlign: 'center',
+    fontSize: 30
   },
   roomCodeCont: {
-    flex: 1,
+    flex: 1
   },
   roomCodeText: {
-    fontFamily: "Graduate",
-    textAlign: "center",
-    fontSize: 25,
+    fontFamily: 'Graduate',
+    textAlign: 'center',
+    fontSize: 25
   },
 
   participantsCont: {
-    flex: 1,
+    flex: 1
   },
   participantsText: {
-    fontFamily: "Graduate",
-    fontSize: 20,
+    fontFamily: 'Graduate',
+    fontSize: 20
   },
 
   // TABLE SECTION
 
   waitingTable: {
     flex: 3,
-    justifyContent: "flex-start",
-    textAlign: "center",
+    justifyContent: 'flex-start',
+    textAlign: 'center'
   },
 
   waitingText: {
     paddingTop: 12,
-    textAlign: "center",
-    fontFamily: "Graduate",
-    fontSize: 20,
+    textAlign: 'center',
+    fontFamily: 'Graduate',
+    fontSize: 20
   },
 
   // User Card
 
   card: {
-    borderColor: "#ED2E18",
+    borderColor: '#ED2E18',
     borderWidth: 2,
-    backgroundColor: "#F7A919",
+    backgroundColor: '#F7A919',
     paddingBottom: 0.2,
-    flexDirection: "row",
-    height: 100,
+    flexDirection: 'row',
+    height: 100
   },
 
   // flex horizontal
 
   cardImage: {
-    flex: 1,
+    flex: 1
   },
   cardText: {
     top: -4.5,
     marginLeft: 75,
     flex: 4,
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    textAlign: "left",
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    textAlign: 'left'
   },
 
   cardTitle: {
-    fontFamily: "Graduate",
-    textTransform: "uppercase",
+    fontFamily: 'Graduate',
+    textTransform: 'uppercase'
   },
   hostTitle: {
-    fontFamily: "Graduate",
+    fontFamily: 'Graduate'
   },
   highScore: {
-    fontFamily: "Graduate",
+    fontFamily: 'Graduate'
   },
   arguText: {
-    fontFamily: "Graduate",
+    fontFamily: 'Graduate'
   },
   avatar: {
     width: 50,
-    height: 50,
-  },
+    height: 50
+  }
 });
